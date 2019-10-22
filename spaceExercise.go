7712bfa23e51e101749ccc828cb2e9874a3eb15d
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-10-21 21:54:54 -0700

    Implements complete functionality.

--- spaceExercise.go
+++ spaceExercise.go
@@ -2,17 +2,30 @@ package main
 
 import (
   "fmt"
-  rand "math/rand"
+  "math/rand"
   "time"
+  "os"
+  "encoding/json"
+  "io/ioutil"
+  "strings"
 )
 
 func main() {
-  seed := rand.NewSource(time.Now().UnixNano())
-  newrand := rand.New(seed)
-  var x = newrand.Intn(10)
-  fmt.Print(x)
 
-  /*
+  planetsjson, err := os.Open(os.Args[1])
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  jsondata, _ := ioutil.ReadAll(planetsjson)
+  var planets Planets
+
+  json.Unmarshal(jsondata, &planets)
+  for i := 0; i < len(planets.Planets); i++ {
+    fmt.Println("Planet Name: " + planets.Planets[i].Name)
+  }
+
+
   fmt.Println("Welcome to the Solar System!")
   fmt.Println("There are 9 planets to explore.")
   fmt.Println("What is your name?")
@@ -25,14 +38,48 @@ func main() {
     var choose string
     fmt.Scanln(&choose)
     if choose == "Y" {
-      fmt.Println("Okay, traveling to RANDOM")
+      seed := rand.NewSource(time.Now().UnixNano())
+      newrand := rand.New(seed)
+      var randomindex = newrand.Intn(10)
+
+      fmt.Println("Okay, traveling to " + planets.Planets[randomindex].Name + "...")
+      fmt.Println("Arrived at " + planets.Planets[randomindex].Name + ". " + planets.Planets[randomindex].Description)
       break;
     } else if choose == "N" {
       fmt.Println("What planet would you like to travel to?")
+      for true {
+        var planet string
+        fmt.Scanln(&planet)
+        planet = strings.ToLower(planet)
+        planet = strings.Title(planet)
+        var exists bool
+        var planetindex int
+        for i := 0; i < len(planets.Planets); i++ {
+          if planets.Planets[i].Name == planet {
+            exists = true;
+            planetindex = i;
+          }
+        }
+        if exists {
+          fmt.Println("Traveling to " + planet + "...")
+          fmt.Println("Arrived at " + planet + ". " + planets.Planets[planetindex].Description)
+          break;
+        } else {
+          fmt.Println("That planet doesn't exist.")
+        }
+      }
       break;
     } else {
       fmt.Println("Excuse me?")
     }
   }
-  */
+}
+
+type Planets struct {
+  Planets []Planet `json:"planets"`
+}
+
+type Planet struct {
+  Name string `json:"name"`
+  Description string `json:"description"`
 }
